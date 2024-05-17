5b9ba2d7eb81561c622e3c34760392a9ba01e154
K9-guardian <girkar.km@gmail.com>
2024-05-17 01:28:57 -0700

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-#!/bin/bash 
+#!/bin/sh 
 ssh origami@acsweb.ucsd.edu "tar -czf public_html.gz public_html"
 ssh origami@acsweb.ucsd.edu "rm -rf public_html"
-mkdocs build
-scp -r site origami@acsweb.ucsd.edu:~/public_html
+bundle exec jekyll build
+scp -r _site origami@acsweb.ucsd.edu:~/public_html
